b63cabc79eb07d7b9e82e5d179706169e1d88ad1
Simona Tsenova <simona.tsenova17@gmail.com>
2017-10-20 09:35:56 +0300

    Changed colors

--- Cars.Web/App_Start/RouteConfig.cs
+++ Cars.Web/App_Start/RouteConfig.cs
@@ -8,6 +8,7 @@ namespace Cars.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
             routes.MapRoute(
                 name: "Default",
